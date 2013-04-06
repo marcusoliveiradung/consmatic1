cadd3b4923c2d085f9d0a91f40764dd51a8f41b1
Gustavo Rahal <grahal@me.com>
2013-04-06 18:33:06 +0000

    JHFKJDKJDHFKSJ

--- consmatic/settings.py
+++ consmatic/settings.py
@@ -20,10 +20,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
         'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'consmatic',                   # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+#       'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+	'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recons',                   # Or path to database file if using sqlite3.
+        'USER': 'recons',                      # Not used with sqlite3.
+        'PASSWORD': 'b1229dfa',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
--- projeto_cineo_acerto/admin.py
+++ projeto_cineo_acerto/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
 # from corpus - app
-from projeto_cineo_acerto.models import   Idioma, Variavel, Termo_ou_Fraseologismo, Conteudo, Entrada #Autor
+from projeto_cineo_acerto.models import   Idioma, Variavel, Termo_ou_Fraseologismo, Conteudo, Entrada, Autor
 #****************
 #*****************
 class EntradaInline(admin.StackedInline):
      fieldsets = [
-     ('Info Data', {'fields': ['variavel','termo','idioma','conteudo'], 'classes': ['collapse']}),
+     ('Info Data', {'fields': ['variavel','termo','conteudo'], 'classes': ['collapse']}),
      ]   
      model = Entrada
      extra = 3
@@ -19,7 +19,7 @@ class Termo_ou_FraseologismoAdmin(admin.ModelAdmin):
    
 #******************************************
 admin.site.register(Variavel)
-#admin.site.register(Autor)
+admin.site.register(Autor)
 admin.site.register(Idioma)
 admin.site.register(Termo_ou_Fraseologismo,Termo_ou_FraseologismoAdmin)
 admin.site.register(Entrada)#,EntradaAdmin)
