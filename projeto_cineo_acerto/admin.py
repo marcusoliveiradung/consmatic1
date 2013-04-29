5623206ede568bdd13d82eae3353ed3b4e64d3c4
INFOCOMUNICOLOGIA <marcus.dung@gmail.com>
2013-04-29 10:03:01 -0300

    alters 290413

--- projeto_cineo_acerto/admin.py
+++ projeto_cineo_acerto/admin.py
@@ -1,27 +1,42 @@
 from django.contrib import admin
 
-# from corpus - app
+from holoteca.models  import  Dominiologia_Atribut, Tipo_Atributo, Config_Atribut_ATTR
 from projeto_cineo_acerto.models import  Idioma, Variavel, Termo_ou_Fraseologismo, Conteudo, Entrada #, Autor
 #****************
 #*****************
 class EntradaInline(admin.StackedInline):
+     model =  Entrada
      fieldsets = [
-     ('Info Data', {'fields': ['variavel','termo','conteudo'], 'classes': ['collapse']}),
+     ('Info Data', {'fields': ['termo','variavel', 'conteudo'],#'self.variavel.atributo',
+     'classes': ['collapse']}),
      ]   
-     model = Entrada
-     extra = 3
+#**************************
+#class DominioInline(admin.StackedInline):
+#      model =  Dominiologia_Atribut
+#     fieldsets = [
+#      ('Info Data', {'fields': ['variavel.','conteudo'],
+#      'classes': ['collapse']}),
+#      ]   
+    
 #********************
 class Termo_ou_FraseologismoAdmin(admin.ModelAdmin):
-   
+     model =  Termo_ou_Fraseologismo #, Config_Atribut_ATTR]
      inlines = [EntradaInline]
-   
-    
+     #inlines = [DominioInline]
+     extra = 3
+
+ #********************
+ #class Atributo_Admin(admin.ModelAdmin):
+ #   model = Termo_ou_Fraseologismo #, Config_Atribut_ATTR]
+ #    inlines = [DominioInline]
+ #    extra = 3
    
 #******************************************
 admin.site.register(Variavel)
 #admin.site.register(Autor)
 admin.site.register(Idioma)
 admin.site.register(Termo_ou_Fraseologismo,Termo_ou_FraseologismoAdmin)
-admin.site.register(Entrada)#,EntradaAdmin)
-admin.site.register(Conteudo)
+#admin.site.register(Atributo_Admin)
+#admin.site.register(Entrada)#,EntradaAdmin)
+#admin.site.register(Conteudo)
 #********************************
