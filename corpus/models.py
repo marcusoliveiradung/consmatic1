5384fe82c1ed7daacba9090d042b91cda27c57c8
marcusoliveiradung <marcus.dung@gmail.com>
2013-09-04 03:26:40 -0300

    Update models.py

--- corpus/models.py
+++ corpus/models.py
@@ -35,10 +35,9 @@ class Escala_Evolutiva(models.Model):
 #das Especialidades:
 class Especialidade(models.Model):
     nome  = models.CharField(max_length=200) 
-     
     descricao = models.CharField(max_length=300)  
     ordem_logica = models.IntegerField()
-    espec_assoc = models.ForeignKey("self",blank= 'True', null='True')
+    espec_sup = models.ForeignKey("self",blank= 'True', null='True')
     #espec_assoc = models.ManyToManyField("self",blank= 'True', null='True') 
     def __unicode__(self):
         return self.nome
