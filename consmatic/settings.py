0049277a14c93d19fdbfad1be01b8efe7294ca8f
marcusoliveiradung <marcus.dung@gmail.com>
2013-09-10 00:38:33 -0300

    Update settings.py

--- consmatic/settings.py
+++ consmatic/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/marcus/Documentos/workspace-sts-2.9.1.RELEASE/consmatic/templates/',
+    #'/home/marcus/Documentos/workspace-sts-2.9.1.RELEASE/consmatic/templates/',
     '/home/recons/webapps/intranet/consmatic/',
   )
 
