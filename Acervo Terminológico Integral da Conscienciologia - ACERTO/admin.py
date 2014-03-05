3495a544ddc51c0cb0b9e4cc15a542122441278b
marcusoliveiradung <marcus.dung@gmail.com>
2014-03-05 19:44:15 -0300

    Update admin.py

--- Acervo Terminológico Integral da Conscienciologia - ACERTO/admin.py
+++ Acervo Terminológico Integral da Conscienciologia - ACERTO/admin.py
@@ -1,6 +1,7 @@
 # _*_ coding: utf8 _*_
 from django.contrib import admin
 
+from corpus.models import Especialidade
 from holoteca.models  import  Dominiologia_Atribut, Tipo_Atributo, Config_Atribut_ATTR
 from projeto_cineo_acerto.models import  Idioma, Variavel, Termo_ou_Fraseologismo,  Entrada 
 #from 'Acervo Terminológico Integral da Conscienciologia - ACERTO'.models import  Idioma, Variavel, Termo_ou_Fraseologismo,  Entrada 
@@ -13,6 +14,9 @@ class EntradaInline(admin.StackedInline):
      #'classes': ['collapse']# 
      }),
           ]
+          
+class EspecialidadeInline(admin.StackedInline):
+     model =  Entrada     
         
 #**************************
 #class DominioInline(admin.StackedInline):
@@ -45,7 +49,14 @@ class Termo_ou_FraseologismoAdmin(admin.ModelAdmin):
      inlines = [EntradaInline]
      #ordering = ['sequencia']
      extra = 3
-
+#*************************
+class Termo_EspecialidadeAdminou_FraseologismoAdmin(admin.ModelAdmin):
+     model =  Termo_ou_Fraseologismo #, Config_Atribut_ATTR]
+     #search_fields = ['nome']
+     #list_filter = ['idioma_orig','especialidade_central'] 
+     inlines = [EspecialidadeInline]
+     #ordering = ['sequencia']
+     extra = 10
  #********************
  #class Atributo_Admin(admin.ModelAdmin):
  #   model = Termo_ou_Fraseologismo #, Config_Atribut_ATTR]
@@ -57,6 +68,7 @@ admin.site.register(Variavel, VariavelAdmin)
 #admin.site.register(Autor)
 admin.site.register(Idioma)
 admin.site.register(Termo_ou_Fraseologismo,Termo_ou_FraseologismoAdmin)
+admin.site.register(Especialidade, Termo_EspecialidadeAdmin)
 #admin.site.register(Atributo_Admin)
 #admin.site.register(Entrada)#,EntradaAdmin)
 #admin.site.register(Conteudo)
