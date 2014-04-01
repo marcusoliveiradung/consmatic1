67d317461e07b0ba2c922b8efcf7044d67b8974c
marcusoliveiradung <marcus.dung@gmail.com>
2014-04-01 07:17:00 -0300

    Update admin.py

--- corpus/admin.py
+++ corpus/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 # from corpus - app
 from corpus.models import  Especialidade, Tema, Escala_Evolutiva, Faixas_Etaria, Procedex, Hominologia
 #****************
-class EspecialidadeAdmin(admin.ModelAdmin):
-     model = Especialidade
-     #inlines = [EspecialidadeInline]
-     extra = 3
-     search_fields = ['nome']
-     list_filter = ['ordem_logica','ind_subespec', 'espec_sup'] 
+#class EspecialidadeAdmin(admin.ModelAdmin):
+#     model = Especialidade
+#     #inlines = [EspecialidadeInline]
+#     extra = 3
+#     search_fields = ['nome']
+#     list_filter = ['ordem_logica','ind_subespec', 'espec_sup'] 
 #********************************
 #admin.site.register(Especialidade) #, EspecialidadeAdmin)
 admin.site.register(Escala_Evolutiva)
